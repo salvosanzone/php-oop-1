454b7c12cd60879dbf23469500bfac7e954016a1
salvosanzone <salvosansone.ss@gmail.com>
2022-01-17 15:01:01 +0100

    inserimento style

--- index.php
+++ index.php
@@ -8,10 +8,12 @@
   //aggiungo una proprietà senza utilizzare il costruttore
   $film1->dop = 'Tonino Delli Colli';
 
-  // $film2 = new Movie();
-  // $film2->title = 'Stalker';
-  // $film2->director = 'Andrej Tarkovskij';
-  // $film2->year = 1979;
+  //inserisco le proprietà di base presenti nel costruttore
+  $film2 = new Movie('Stalker', 'Andrej Tarkovskij', 1979, 'Sci-fi');
+
+  //aggiungo una proprietà senza utilizzare il costruttore
+  $film2->dop = 'Aleksandr Kniažinskij';
+  
 ?>
 
 <!DOCTYPE html>
@@ -21,11 +23,12 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
+  <link rel="stylesheet" href="./style/style.css">
 </head>
 <body>
 
 <!-- FILM -->
-  <div>
+  <div class="film">
     <ul>
       <h2>Title of Movie:</h2>
       <li>
@@ -55,6 +58,43 @@
       <li>
         <h4>
           <?php echo $film1->dop; ?>
+        </h4>
+      </li>
+    </ul>
+  </div> 
+<!-- /FILM -->
+
+<!-- FILM -->
+  <div class="film">
+    <ul>
+      <h2>Title of Movie:</h2>
+      <li>
+        <h4>
+          <?php echo $film2->title; ?>
+        </h4>
+      </li>
+      <h2>Director:</h2>  
+      <li>
+        <h4>
+          <?php echo $film2->director; ?>
+        </h4>
+      </li>
+      <h2>Year:</h2>  
+      <li>
+        <h4>
+          <?php echo $film2->year; ?>
+        </h4>
+      </li>
+      <h2>Genre:</h2>
+      <li>
+        <h4>
+          <?php echo $film2->getParentControl(); ?>
+        </h4>
+      </li>
+      <h2>Director Of Photography:</h2>
+      <li>
+        <h4>
+          <?php echo $film2->dop; ?>
         </h4>
       </li>
     </ul>
